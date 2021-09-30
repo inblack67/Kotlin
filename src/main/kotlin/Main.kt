6d15f2cb4244a622586e27b7a69d83510dbb6dc6
aman <aman>
2021-09-30 23:53:25 +0530

    cont.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,26 @@
 fun main() {
-    print("ok")
+    println("hello worlds")
     val read = "immutable"
-//    read = "nah"
-    var change = 1
-    change = 2
-    print("$read and $change")
-    var a: Int = 20
+    var write = "mutable"
+    write = "mutated"
+    println("$read and $write")
+    val a: Int = 10 // Int, Float, Double, Long (whole)
+    println(a)
+//    println(true is Boolean)
+//    println(false is Boolean)
+    val letter: Char = 'A'
+    println("$letter => ${letter.code}") // 65
+    val str: String = "I am a string"
+    println(str)
+    val multiString = """
+        hello
+            ok
+        """
+    println(multiString)
+    println("${1+1}")
+    val intel = "do you know gods of death love apples"
+//    println(intel.get(1))
+    println(intel[0])
+    println(intel.subSequence(1,3))
+    println(intel.contains("apples"))
 }

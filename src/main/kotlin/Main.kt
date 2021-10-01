6d13ef01c779328f23cde3be69a8f58b24233eb0
aman <aman>
2021-10-01 10:56:14 +0530

    arrays lists and hashmaps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,4 +23,43 @@ fun main() {
     println(intel[0])
     println(intel.subSequence(1,3))
     println(intel.contains("apples"))
+
+    // === => checks by reference
+    val str2 = "hello"
+    val str3 = str2
+    println(str2 === str3)
+    println("hello worlds".contains("hello"))
+    println("ok".length)
+
+//    arrays => fixed size
+    var nums = arrayOf(1,2,3)
+    nums[0] = 10
+    println(nums)
+    println(nums.size)
+    nums += 12
+    println(nums.toList())
+    val arr = Array(5) { i -> i * i }
+    println(arr.toList())
+
+//    lists => var number of elements
+    val fruits = mutableListOf<String>("apple", "mango")
+    fruits.add("some fruit")
+    fruits.removeAt(1)
+
+    val immutableList = listOf<String>("pure", "fp")
+
+//    sets => every elem unique
+    val mySet = mutableSetOf<Char>('A','B','A')
+    println(mySet)
+    mySet.add('B')
+    mySet.remove('A')
+//    indexes wont work
+
+//    hashmaps
+    val map: HashMap<Int, String> = hashMapOf(1 to "a", 2 to "b", 3 to "c")
+    println(map)
+    println(map[2])
+    map[1] = "ok"
+    map.remove(2)
+    map[3] = "new kv"
 }
